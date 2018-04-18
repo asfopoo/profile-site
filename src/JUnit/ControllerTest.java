cae436f44b05cdef27ac34ac9f16ffe975b866cf
Edward Nardo <enardo@ycp.edu>
2018-04-18 10:47:27 -0400

    JUnit

--- src/JUnit/ControllerTest.java
+++ src/JUnit/ControllerTest.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import entity.Area;
@@ -20,23 +23,31 @@ import model.Game;
 public class ControllerTest {
 	
 	private IDatabase db = null;
-	private Item lighter;
+	ArrayList<Item> items = null;
+	ArrayList<Area> areas = null;
+	private Item lighter = new Item(2, "greenLighter", "utility");
 	
-
-	
-	//setup
-	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
 	@Before
 	public void setUp() throws Exception {
 		DatabaseProvider.setInstance(new DerbyDatabase()); // some of this code taken from lab 06 and library example-- CITING
 		 db = DatabaseProvider.getInstance();
 	}
-	
+	@After
+	public void tearDown() throws Exception {
+	}
+
 	
 	
 	@Test
 	public void testInsertUserItem(int size, String name, String type) { 
-	
+		
+		//db.insertUserItem(2, "greenLighter", "utility");
 	}
 	@Test
 	public void testRemoveItem(int size, String name, String type) {
